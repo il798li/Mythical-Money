575fc3782211dea39db7be8e2837f17171f51d36
il798li <92280179+il798li@users.noreply.github.com>
2024-02-28 21:56:53 -0800

    Delete thing.py

--- thing.py
+++
@@ -1,5 +0,0 @@
-import pyautogui as pag #in other IDEs, this will be auto-imported, so this line usually doesnt exist
-pag.PAUSE = 0.5 #technically this doesnt count. pause is usually 0.1 but thats too fast for my laptop
-while True:
-    pag.write('update')
-    pag.press('enter')
